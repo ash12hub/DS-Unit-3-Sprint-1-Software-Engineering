4bb3c7399a17810c7add49af52418df7aa64164e
ash12hub <ash12hub@users.noreply.github.com>
2019-09-07 09:55:43 +0530

    assignment

---
+++ module2-oop-code-style-and-reviews/oop_lecture.py
@@ -0,0 +1,34 @@
+'''
+DSPT1-Lecture 312-OOP and Code Style
+'''
+from random import random
+
+
+class Game:
+    """General representation of a game"""
+
+    def __init__(self, player1='A', player2='B'):
+        self.round = 1
+        self.player1 = player1
+        self.player2 = player2
+
+    def print_players(self):
+        """Print the players of the game"""
+        print('{} is playing {}'.format(self.player1, self.player2))
+
+    def add_rounds(self):
+        """Incrementing the number of rounds by 1"""
+        self.round += 1
+
+    def winner(self):
+        return(self.player1 if random() > 0.5 else self.player2)
+
+
+class TicTacToe(Game):
+    def __init__(self, player1='Joe', player2='Bill'):
+        super().__init__(player1, player2)
+
+    def print_players(self):
+        """Print the players of the game"""
+        print('{} is playing Tic-Tac-Toe with {}'.format(
+            self.player1, self.player2))
